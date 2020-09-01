f3cad6ca4be9b346d4cb5c33e3590e4f4a369aea
root <zhaokevinma@outlook.com>
2020-09-01 06:47:49 +0000

    process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,36 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// DONE: Return this process's ID
+int Process::Pid() { return this->pid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// DONE: Return this process's CPU utilization
+float Process::CpuUtilization() const{ 
+  return (LinuxParser::ActiveJiffies(this->pid_)/sysconf(_SC_CLK_TCK))
+    /(float)LinuxParser::UpTime(this->pid_); }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// DONE: Return the command that generated this process
+string Process::Command() { return LinuxParser::Command(this->pid_); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// DONE: Return this process's memory utilization
+string Process::Ram() { return LinuxParser::Ram(this->pid_); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// DONE: Return the user (name) that generated this process
+string Process::User() { return LinuxParser::User(this->pid_); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// DONE: Return the age of this process (in seconds)
+long int Process::UpTime() { return LinuxParser::UpTime(this->pid_); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+// DONE: Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const& a) const { 
+  if(this->CpuUtilization()<a.CpuUtilization())
+    return true;
+  else
+    return false;
+}
